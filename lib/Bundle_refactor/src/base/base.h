65e8ada727266d90150ef8e635978479069190c3
jbw9964 <jbw9964@gmail.com>
2023-11-01 03:46:43 +0900

    refactor: refactored codes of `Bundle` module
    refactored codes
    - optimized work flow
    - organized variables
    - add comments (still on progress)

---
+++ lib/Bundle_refactor/src/base/base.h
@@ -0,0 +1,67 @@
+
+# ifndef __Bundle_base__
+# define __Bundle_base__
+
+# include <Arduino.h>
+
+class Basic_port
+{
+    protected:
+        int Port;
+
+        void output_setup();
+        void input_setup();
+        void input_pullup_setup();
+
+        void write_digital(int);
+        void write_analog(int);
+        int read_digital();
+        int read_analog();
+
+        void print_port();
+    
+    public:
+        Basic_port();
+        Basic_port(int);
+};
+
+class LED : public Basic_port
+{
+    private:
+        void setup();
+    
+    public:
+        LED();
+        LED(int);
+        ~LED();
+
+        void assign_port(int);
+        void write_low();
+        void write_high();
+        void print_port();
+};
+
+class Sensor : public Basic_port
+{
+    private:
+        int Prev_status;
+
+        void setup();
+        int read();
+
+    public:
+        Sensor();
+        Sensor(int);
+
+        void assign_port(int);
+        bool is_received();
+
+        void print_port();
+};
+
+
+# ifndef __Bundle_base_func__
+    # include <base/base.cpp>
+# endif
+
+# endif
---
+++ lib/Bundle_refactor/src/bundle_refactor.h
@@ -0,0 +1,7 @@
+
+# ifndef __Bundle_refactor__
+# define __Bundle_refactor__
+    # include <base/base.h>                                 // Dependency   :   None
+    # include <sensor_LED_bundle/sensor_LED_bundle.h>       // Dependency   :   None
+    # include <sensor_servo_bundle/sensor_servo_bundle.h>   // Dependency   :   <Servo.h>
+# endif
